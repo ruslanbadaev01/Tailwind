61966efe7d5e4bb2bb8cfabdcecb051a93f57d52
L0c_d0G <94112336+ruslanbadaev01@users.noreply.github.com>
2022-12-29 19:19:31 +0300

    Header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    screens: {
+      lg: { max: "992px" },
+      md: { max: "768px" },
+      sm: { max: "480px" },
+    },
+    container: {
+      padding: "20px",
+    },
+    extend: {
+      colors: {
+        black: "#4d4244",
+        red: "#ff0d38",
+        darkred: "#d70026",
+        gray: "#747474",
+        darkgray: "#272727",
+      },
+    },
   },
   plugins: [],
 };
