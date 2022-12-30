55ad278a71f49b1c868945ab172980fc40d0c225
L0c_d0G <94112336+ruslanbadaev01@users.noreply.github.com>
2022-12-30 17:43:33 +0300

    Finish

--- js/main.js
+++ js/main.js
@@ -10,3 +10,20 @@ function burgerMenu() {
 }
 
 burgerMenu();
+
+//Dark Mode
+
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  document.documentElement.classList.add("dark");
+} else {
+  document.documentElement.classList.remove("dark");
+}
+
+const modeBtn = document.querySelector("#mode");
+modeBtn.addEventListener("click", () => {
+  document.querySelector("html").classList.toggle("dark");
+});
