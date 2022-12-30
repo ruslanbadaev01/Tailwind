1ec8b459c901ddbf8233ee14c9a886c9bebc7a4f
L0c_d0G <94112336+ruslanbadaev01@users.noreply.github.com>
2022-12-30 16:39:44 +0300

    First Block

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,9 @@ module.exports = {
         gray: "#747474",
         darkgray: "#272727",
       },
+      boxShadow: {
+        customRed: "0px 0px 30px rgba(255, 13, 56, 0.21)",
+      },
     },
   },
   plugins: [],
